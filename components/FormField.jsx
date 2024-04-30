00d4426cdd609fb6523083c072fb90161b581ccf
Prithiraj Talukder <PrithirajTalukder@gmail.com>
2024-05-01 00:55:20 +0600

    modified:   app/(auth)/sign-in.jsx 	modified:   app/(auth)/sign-up.jsx 	modified:   app/_layout.jsx 	modified:   components/FormField.jsx

--- components/FormField.jsx
+++ components/FormField.jsx
@@ -1,5 +1,8 @@
-import { View, Text, TextInput } from 'react-native';
+import { View, Text, TextInput, TouchableOpacity, Image } from 'react-native';
 import React, { useState } from 'react';
+
+
+import {icons} from '../constants'
 
 const FormField = ({ title, value, placeholder, handleChangeText, otherStyles, ...props }) => {
   
@@ -9,7 +12,7 @@ const FormField = ({ title, value, placeholder, handleChangeText, otherStyles, .
     <View className={`space-y-2 ${otherStyles}`}>
       <Text className="text-base text-gray-100 font-pmedium">{title}</Text>
 
-      <View className="border-2 border-black-200 w-full h-16 px-4 bg-black-100 rounded-2xl focus:border-secondary items-center">
+      <View className="border-2 border-black-200 w-full h-16 px-4 bg-black-100 rounded-2xl focus:border-secondary items-center flex-row">
 
         <TextInput 
           className="flex-1 text-white font-psemibold text-base"
@@ -19,6 +22,20 @@ const FormField = ({ title, value, placeholder, handleChangeText, otherStyles, .
           onChangeText={handleChangeText}
           secureTextEntry={title === "Password" && !showPassword}
         />
+
+        {title === 'Password' && (
+          <TouchableOpacity onPress={() =>
+          setShowPassword(!showPassword)}>
+
+            <Image 
+            
+            source={!showPassword ? icons.eye : icons.eyehide} className="w-6 h-6" resizeMode='contain'/>
+
+          </TouchableOpacity>
+        )}
+
+
+
       </View>
     </View>
   );
