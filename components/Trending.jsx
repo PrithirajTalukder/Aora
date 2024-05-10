54e459c8e9cf6a496402b6fafdc7b54e0912714a
Prithiraj Talukder <PrithirajTalukder@gmail.com>
2024-05-11 01:13:22 +0600

    modified:   components/Trending.jsx 	modified:   components/VideoCard.jsx

--- components/Trending.jsx
+++ components/Trending.jsx
@@ -2,13 +2,14 @@ import { useState } from 'react';
 import { View, Text, FlatList, TouchableOpacity, ImageBackground, Image } from 'react-native'
 import * as Animatable from 'react-native-animatable';
 import { icons } from '../constants';
+import { ResizeMode, Video } from 'expo-av';
 
 const zoomIn ={
   0:{
     scale: 0.9
   },
   1:{
-    scale: 1,
+    scale: 1.1,
   }
 }
 
@@ -31,7 +32,18 @@ const TrendingItem = ({activeItem, item}) =>{
     duration={500}>
 
       {play ? (
-        <Text className="text-white">Playing</Text>
+       <Video 
+       source={{uri: item.video}}
+       className="w-52 h-72 rounded-[35px] mt-3 bg-white/10"
+       resizeMode={ResizeMode.CONTAIN}
+       useNativeControls
+       shouldPlay
+       onPlaybackStatusUpdate={(status) =>{
+        if(status.didJustFinish){
+          setPlay(false);
+        }
+       }}
+       />
       ): (
         <TouchableOpacity className="relative justify-center items-center"
         activeOpacity={0.7}
@@ -58,7 +70,13 @@ const TrendingItem = ({activeItem, item}) =>{
 
 const Trending = ({posts}) => {
   console.log('Trending posts:', posts); 
-  const [activeItem, setActiveItem] = useState(posts[0]);
+  const [activeItem, setActiveItem] = useState(posts[1]);
+
+  const viewableItemsChanged = ({viewableItems}) =>{
+    if(viewableItems.length > 0){
+      setActiveItem(viewableItems[0].key)
+    }
+  }
   return (
     <FlatList
     data={posts}
@@ -69,6 +87,13 @@ const Trending = ({posts}) => {
         />
         
     )}
+    onViewableItemsChanged={viewableItemsChanged}
+    viewabilityConfig={{
+      itemVisiblePercentThreshold:70
+    }}
+
+    contentOffset={ { x: 170 } }
+
 
     horizontal
     
