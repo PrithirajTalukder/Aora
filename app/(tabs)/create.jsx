3e1d06d6da7e66e71e4ccf557c32a186d8db3ca5
Prithiraj Talukder <PrithirajTalukder@gmail.com>
2024-05-25 00:52:35 +0600

    modified:   app/(tabs)/create.jsx

--- app/(tabs)/create.jsx
+++ app/(tabs)/create.jsx
@@ -1,11 +1,26 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { ScrollView, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import FormField from '../../components/FormField'
 
 const Create = () => {
   return (
-    <View>
-      <Text>Create</Text>
-    </View>
+    <SafeAreaView className= "bg-primary h-full">
+      <ScrollView className="px-4 my-6">
+
+      
+      <Text className="text-2xl text-white font-psemibold">
+
+        Upload Video
+
+
+      </Text>
+
+      <FormField 
+      title ="Video Title"/>
+
+      </ScrollView>
+    </SafeAreaView>
   )
 }
 
